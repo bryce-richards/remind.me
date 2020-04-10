25f669d92412ecd2dc2298ff9f76948f8fb10548
Bryce Richards <>
2020-04-10 12:12:01 -0700

    update server index.js

--- index.js
+++ index.js
@@ -40,9 +40,9 @@ app.use('/verify', verifyRoutes);
 app.use('/api', passport.authenticate('jwt', { session : false }), reminderRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
   const path = require('path');
+
+  app.use(express.static(path.join(__dirname, '/client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
